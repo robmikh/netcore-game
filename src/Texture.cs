5b98d1e4f1c36c4b62f23bad5ff2375dd195f979
robmikh <rob.mikh@outlook.com>
2016-12-10 23:24:42 -0800

    Fix texture coordinate filp

--- src/Texture.cs
+++ src/Texture.cs
@@ -23,24 +23,32 @@ namespace netcoregame
             GL.BindTexture(TextureTarget.Texture2D, Id);
             GL.Begin(PrimitiveType.Quads);
 
+            // Texture Coordnates:
             //  0 --> 1
             //  ^     |
             //  |     |
             //  |     V
             //  3 <-- 2
 
+            // Vertices:
+            //  3 <-- 2
+            //  |     ^
+            //  |     |
+            //  V     |
+            //  0 --> 1
+
             // 0
             GL.TexCoord2((float)sourceRect.Left / Width, (float)sourceRect.Top / Height);
-            GL.Vertex2((((float)destRect.Left / context.CanvasWidth) * 2) - 1, (((float)destRect.Top / context.CanvasHeight) * 2) - 1);
+            GL.Vertex2((((float)destRect.Left / context.CanvasWidth) * 2) - 1, (((float)destRect.Bottom / context.CanvasHeight) * 2) - 1);
             // 1
             GL.TexCoord2((float)sourceRect.Right / (float)Width, (float)sourceRect.Top / Height);
-            GL.Vertex2((((float)destRect.Right / context.CanvasWidth) * 2) - 1, (((float)destRect.Top / context.CanvasHeight) * 2) - 1);
+            GL.Vertex2((((float)destRect.Right / context.CanvasWidth) * 2) - 1, (((float)destRect.Bottom / context.CanvasHeight) * 2) - 1);
             // 2
             GL.TexCoord2((float)sourceRect.Right / Width, (float)sourceRect.Bottom / Height);
-            GL.Vertex2((((float)destRect.Right / context.CanvasWidth) * 2) - 1, (((float)destRect.Bottom / context.CanvasHeight) * 2) - 1);
+            GL.Vertex2((((float)destRect.Right / context.CanvasWidth) * 2) - 1, (((float)destRect.Top / context.CanvasHeight) * 2) - 1);
             // 3
             GL.TexCoord2((float)sourceRect.Left / Width, (float)sourceRect.Bottom / Height);
-            GL.Vertex2((((float)destRect.Left / context.CanvasWidth) * 2) - 1, (((float)destRect.Bottom / context.CanvasHeight) * 2) - 1);
+            GL.Vertex2((((float)destRect.Left / context.CanvasWidth) * 2) - 1, (((float)destRect.Top / context.CanvasHeight) * 2) - 1);
 
             GL.End();
             GL.Disable(EnableCap.Texture2D);
